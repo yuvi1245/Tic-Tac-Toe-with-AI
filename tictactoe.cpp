e144de172843fe27d13c7f8a814952254388927c
yuvi1245 <yuvasairaj1919@gmail.com>
2026-04-06 13:13:26 +0530

    Update tictactoe.py

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,137 +1,106 @@
-#include <iostream>
-using namespace std;
+board = [' ' for _ in range(9)]
 
-char board[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+def print_board():
+    print()
+    print(f" {board[0]} | {board[1]} | {board[2]}")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]}")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]}")
+    print()
 
-void printBoard() {
-    cout << "\n";
-    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << "\n";
-    cout << "---+---+---\n";
-    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << "\n";
-    cout << "---+---+---\n";
-    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << "\n\n";
-}
+def check_win(player):
+    win_positions = [
+        (0,1,2), (3,4,5), (6,7,8),
+        (0,3,6), (1,4,7), (2,5,8),
+        (0,4,8), (2,4,6)
+    ]
+    for a, b, c in win_positions:
+        if board[a] == player and board[b] == player and board[c] == player:
+            return True
+    return False
 
-bool checkWin(char player) {
-    int winPositions[8][3] = {
-        {0,1,2}, {3,4,5}, {6,7,8},
-        {0,3,6}, {1,4,7}, {2,5,8},
-        {0,4,8}, {2,4,6}
-    };
+def is_draw():
+    return ' ' not in board
 
-    for(int i = 0; i < 8; i++) {
-        if(board[winPositions[i][0]] == player &&
-           board[winPositions[i][1]] == player &&
-           board[winPositions[i][2]] == player) {
-            return true;
-        }
-    }
-    return false;
-}
+def minimax(is_ai):
+    if check_win('O'):
+        return 1
+    if check_win('X'):
+        return -1
+    if is_draw():
+        return 0
 
-bool isDraw() {
-    for(int i = 0; i < 9; i++) {
-        if(board[i] == ' ')
-            return false;
-    }
-    return true;
-}
+    if is_ai:
+        best_score = -1000
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'O'
+                score = minimax(False)
+                board[i] = ' '
+                best_score = max(best_score, score)
+        return best_score
+    else:
+        best_score = 1000
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = 'X'
+                score = minimax(True)
+                board[i] = ' '
+                best_score = min(best_score, score)
+        return best_score
 
-int minimax(bool isAI) {
-    if(checkWin('O')) return 1;
-    if(checkWin('X')) return -1;
-    if(isDraw()) return 0;
+def ai_move():
+    best_score = -1000
+    best_move = -1
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = 'O'
+            score = minimax(False)
+            board[i] = ' '
+            if score > best_score:
+                best_score = score
+                best_move = i
+    board[best_move] = 'O'
 
-    if(isAI) {
-        int bestScore = -1000;
-        for(int i = 0; i < 9; i++) {
-            if(board[i] == ' ') {
-                board[i] = 'O';
-                int score = minimax(false);
-                board[i] = ' ';
-                bestScore = max(bestScore, score);
-            }
-        }
-        return bestScore;
-    } else {
-        int bestScore = 1000;
-        for(int i = 0; i < 9; i++) {
-            if(board[i] == ' ') {
-                board[i] = 'X';
-                int score = minimax(true);
-                board[i] = ' ';
-                bestScore = min(bestScore, score);
-            }
-        }
-        return bestScore;
-    }
-}
+def player_move():
+    while True:
+        position = int(input("Enter position (1-9): ")) - 1
+        if 0 <= position < 9 and board[position] == ' ':
+            board[position] = 'X'
+            break
+        else:
+            print("Invalid move! Try again.")
 
-void aiMove() {
-    int bestScore = -1000;
-    int bestMove = -1;
+def main():
+    print("TIC TAC TOE")
+    print("You are X | AI is O")
 
-    for(int i = 0; i < 9; i++) {
-        if(board[i] == ' ') {
-            board[i] = 'O';
-            int score = minimax(false);
-            board[i] = ' ';
-            if(score > bestScore) {
-                bestScore = score;
-                bestMove = i;
-            }
-        }
-    }
-    board[bestMove] = 'O';
-}
+    while True:
+        print_board()
+        player_move()
 
-void playerMove() {
-    int position;
-    while(true) {
-        cout << "Enter position (1-9): ";
-        cin >> position;
-        position--;
-        if(position >= 0 && position < 9 && board[position] == ' ') {
-            board[position] = 'X';
-            break;
-        }
-        cout << "Invalid move! Try again.\n";
-    }
-}
+        if check_win('X'):
+            print_board()
+            print("You win!")
+            break
 
-int main() {
-    cout << "TIC TAC TOE\n";
-    cout << "You are X | AI is O\n";
+        if is_draw():
+            print_board()
+            print("Game Draw!")
+            break
 
-    while(true) {
-        printBoard();
-        playerMove();
+        ai_move()
 
-        if(checkWin('X')) {
-            printBoard();
-            cout << "You win!\n";
-            break;
-        }
+        if check_win('O'):
+            print_board()
+            print("AI wins!")
+            break
 
-        if(isDraw()) {
-            printBoard();
-            cout << "Game Draw!\n";
-            break;
-        }
+        if is_draw():
+            print_board()
+            print("Game Draw!")
+            break
 
-        aiMove();
-
-        if(checkWin('O')) {
-            printBoard();
-            cout << "AI wins!\n";
-            break;
-        }
-
-        if(isDraw()) {
-            printBoard();
-            cout << "Game Draw!\n";
-            break;
-        }
-    }
-    return 0;
-}
+if __name__ == "__main__":
+    main()
